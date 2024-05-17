0179eede81c81891b4ff8ef4514fa4dd8687a5cf
Islam_Nyos <islam_vip02@mail.ru>
2024-05-17 05:21:46 +0500

    Administration

--- financ_administration/src/main/java/com/hakaton/financ_administration/configuration/ApplicationConfig.java
+++ financ_administration/src/main/java/com/hakaton/financ_administration/configuration/ApplicationConfig.java
@@ -1,6 +1,6 @@
 package com.hakaton.financ_administration.configuration;
 
-import kz.message_project.userProject.repositories.UserRepository;
+import com.hakaton.financ_administration.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
