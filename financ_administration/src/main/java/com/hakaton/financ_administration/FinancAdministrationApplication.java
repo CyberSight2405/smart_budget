9883f7fb21754e60397efb474aad29e7708ac05b
Islam_Nyos <islam_vip02@mail.ru>
2024-05-17 05:14:53 +0500

    Administration

---
+++ financ_administration/src/main/java/com/hakaton/financ_administration/FinancAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.hakaton.financ_administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancAdministrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancAdministrationApplication.class, args);
+    }
+
+}
