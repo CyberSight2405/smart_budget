7afa29736a5f37ea7c3065042b2a6af4b72f0dca
Valeriy Emelyanov <emelva@rambler.ru>
2024-05-18 16:23:58 +0300

    fix

--- src/main/java/com/finance/smart_budget/entity/IncomeExpenseItem.java
+++ src/main/java/com/finance/smart_budget/entity/IncomeExpenseItem.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 @Table(name = "income_expense_item")
 public class IncomeExpenseItem {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
